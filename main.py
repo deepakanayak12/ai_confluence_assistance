663c9da2d2aaf46947947cb5577af979909f1084
Deepak Nayak <deepakanayak@gmail.com>
2026-06-23 13:44:35 +0200

    Exception handling

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from utils.logger import get_logger
 import services.confluence as confluence
+import time
 
 logger = get_logger()
 
 def main():
     logger.info("Starting Confluence Assistance Application")
-    confluence_service = confluence.confluenceservice()
-    print(confluence_service)
     
+    max_retries = 3
+
+    for attempt in range(max_retries):
+        try:
+            service = confluence.confluenceservice()
+            logger.info(service)
+            break
+
+        except Exception as e:
+            logger.error(f"Attempt {attempt+1}: {str(e)}")
+
+            if attempt < max_retries - 1:
+                logger.info("Retrying...")
+                time.sleep(2)
+            else:
+                logger.critical("All retries failed", exc_info=True)
+
+
 if __name__ == "__main__":
     main()
--- services/confluence.py
+++ services/confluence.py
@@ -1,8 +1,10 @@
+import random
+
 from utils.logger import get_logger
 
 logger = get_logger()
 
 def confluenceservice():
-    message = "Confluence Service Initialized"
-    logger.info(message)
-    return message
+    if random.choice([True, False]):
+        raise Exception ("Failed to initialize Confluence Service")
+    return "Confluence Service Initialized"
